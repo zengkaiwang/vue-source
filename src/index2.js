ca7553c557f094304e9d8af6e132f0327d061518
scaler <zengkaiwang@yeah.net>
2021-11-12 13:21:22 +0800

    feat: 定义patch()方法用来比对新老虚拟dom更新真实dom&完成了比对标签&比对文本&比对属性的逻辑，正在进行比对孩子逻辑处理，比对孩子有三种情况，新老都要孩子情况下的孩子比对，就是diff算法，正在进行开发

--- src/index2.js
+++ src/index2.js
@@ -1,4 +1,4 @@
-import { h, render } from './vdom'
+import { h, render, patch } from './vdom'
 
 // let app = document.getElementById('app')
 
@@ -7,11 +7,68 @@ import { h, render } from './vdom'
 // 将如下的dom节点,生成虚拟dom
 // <div id="container"><span style="color: red">hello</span>wzk</div>
 
-let oldVnode = h('div', {id: 'container', key: 1, class: 'aa'},
-  h('span', {style: {color: 'red'}}, 'hello'),
-  'wzk'
+// let oldVnode = h('div', {id: 'container', key: 1, class: 'aa'},
+//   h('span', {style: {color: 'red'}}, 'hello'),
+//   'wzk'
+// )
+
+let oldVnode = h('div', {id: 'container'},
+  h('li', {style: {background: 'red'}, key: 'a'}, 'a'),
+  h('li', {style: {background: 'yellow'}, key: 'b'}, 'b'),
+  h('li', {style: {background: 'blue'}, key: 'c'}, 'c'),
+  h('li', {style: {background: 'pink'}, key: 'd'}, 'd'),
 )
+
+// let oldVnode = h('div', {id: 'container', key: 1, class: 'aa'})
 // console.log(oldVnode)
 
 let container = document.getElementById('app')
 render(oldVnode, container)
+
+// 更新操作
+// 尾比头相同
+let newVnode = h('div', {id: 'container'},
+  h('li', {style: {background: 'pink'}, key: 'd'}, 'd'),
+  h('li', {style: {background: 'blue'}, key: 'a'}, 'a'),
+  h('li', {style: {background: 'yellow'}, key: 'b'}, 'b'),
+  h('li', {style: {background: 'red'}, key: 'c'}, 'c'),
+)
+
+// 头比尾相同
+// let newVnode = h('div', {id: 'container'},
+//   h('li', {style: {background: 'pink'}, key: 'd'}, 'd'),
+//   h('li', {style: {background: 'blue'}, key: 'c'}, 'c'),
+//   h('li', {style: {background: 'yellow'}, key: 'b'}, 'b'),
+//   h('li', {style: {background: 'red'}, key: 'a'}, 'a'),
+// )
+
+// 尾比尾相同 新的虚拟节点 在开头多的情况
+// let newVnode = h('div', {id: 'container'},
+//   h('li', {style: {background: 'skyblue'}, key: 'f'}, 'f'),
+//   h('li', {style: {background: 'skyblue'}, key: 'e'}, 'e'),
+//   h('li', {style: {background: 'red'}, key: 'a'}, 'a'),
+//   h('li', {style: {background: 'yellow'}, key: 'b'}, 'b'),
+//   h('li', {style: {background: 'blue'}, key: 'c'}, 'c'),
+//   h('li', {style: {background: 'pink'}, key: 'd'}, 'd'),
+// )
+
+// 头比头相同 新的虚拟节点 在结尾多的情况
+// let newVnode = h('div', {id: 'container'},
+//   h('li', {style: {background: 'red'}, key: 'a'}, 'a'),
+//   h('li', {style: {background: 'yellow'}, key: 'b'}, 'b'),
+//   h('li', {style: {background: 'blue'}, key: 'c'}, 'c'),
+//   h('li', {style: {background: 'pink'}, key: 'd'}, 'd'),
+//   h('li', {style: {background: 'purple'}, key: 'e'}, 'e'),
+// )
+
+// let newVnode = h('p', {id: 'aa'}, 
+//   h('span', {style: {color: 'yellow'}}, 'world'),
+//   'vivian'
+// )
+
+// let newVnode = h('div', {id: 'aa'},)
+
+// patchVnode patch()方法 用新的虚拟节点 和老的虚拟节点做对比 更新真实dom
+setTimeout(() => {
+  patch(oldVnode, newVnode);
+}, 1000)
--- src/vdom/patch.js
+++ src/vdom/patch.js
@@ -2,12 +2,11 @@
 
 // 把虚拟节点 渲染成真实节点
 export function render(vnode, container) {
-  console.log(vnode)
   let el = createElm(vnode);
   container.appendChild(el);
 }
 
-// 创建真实节点
+// 把虚拟dom创建成真实节点, 这是是vue在操作dom
 function createElm(vnode) {
   let { tag, props, key, children, text} = vnode
   if(typeof tag === 'string') {
@@ -30,7 +29,7 @@ function createElm(vnode) {
 
 // 给真实dom 添加或更新属性
 function updateProperties(vnode, oldProps = {}) {
-  let newProps = vnode.props
+  let newProps = vnode.props || {}
   let el = vnode.el
 
   // 更新属性的逻辑
@@ -61,4 +60,118 @@ function updateProperties(vnode, oldProps = {}) {
       el[key] = newProps[key]
     }
   }
+}
+
+export function patch(oldVnode, newVnode) {
+  // 1. 比对标签: 如果标签不一样,直接用新的替换,因为现实使用中用户一般不会只更新父标签
+  if(oldVnode.tag != newVnode.tag) { // 以前是dev 现在是p
+    // 必须拿到当前元素的父亲, 才能替换掉自己
+    oldVnode.el.parentNode.replaceChild(createElm(newVnode), oldVnode.el)
+  }
+
+  // 2. 比较文本, 标签一样, 可能都是undefined
+  if(!oldVnode.tag) {
+    if(oldVnode.text != newVnode.text) { // 如果文本内容不容, 直接用新的元素的内容来替换到文本节点
+      oldVnode.el.textContent = newVnode.text;
+    }
+  }
+
+  // 3. 标签一样的情况
+  // 3.1 比对属性, 可能属性不同
+  let el = newVnode.el = oldVnode.el; // 标签一样 复用即可
+  updateProperties(newVnode, oldVnode.props); // 做属性比对
+
+  // 3.2 比对孩子, 有三种情况
+  let oldChildren = oldVnode.children || []
+  let newChildren = newVnode.children || []
+
+  if(oldChildren.length > 0 && newChildren.length > 0) {
+    // 1) 老的有孩子 新的有孩子 该情况是vue最核心的代码
+    updateChildren(el, oldChildren, newChildren) // 不停的递归比较
+  } else if(oldChildren.length > 0) {
+    // 2) 老的有孩子 新的没孩子
+    el.innerHTML = ''
+  } else if(newChildren.length > 0) { 
+    // 3) 老的没孩子 新的有孩子
+    for(let i = 0; i < children.length; i++) {
+      let child = children[i];
+      el.appendChild(createElm(child)) // 将新的虚拟节点的孩子 循环放到老的节点中即可
+    }
+  }
+
+}
+
+// vue最核心的代码 diff算法 双指针
+function updateChildren(parent, oldChildren, newChildren) {
+  let oldStartIndex = 0; // 老的开始索引
+  let oldStartVnode = oldChildren[0] // 老的开始虚拟节点
+  let oldEndIndex = oldChildren.length -1 // 老的结尾索引
+  let oldEndVnode = oldChildren[oldEndIndex] // 老的结尾虚拟节点
+
+  let newStartIndex = 0; // 新的开始索引
+  let newStartVnode = newChildren[0] // 新的开始虚拟节点
+  let newEndIndex = newChildren.length -1 // 新的结尾索引
+  let newEndVnode = newChildren[newEndIndex] // 新的结尾虚拟节点
+
+  // 新的或老的,孩子列表的 两个指针重合了 就停止while循环
+  // 这里建议画图理解
+  while(oldStartIndex <= oldEndIndex && newStartIndex <= newEndIndex) {
+
+    // 1) 头比头相同 新旧首指针指向的相同
+    if(isSameVnode(oldStartVnode, newStartVnode)) {
+      patch(oldStartVnode, newStartVnode) // 用新的属性更新老的属性
+      oldStartVnode = oldChildren[++oldStartIndex] // 把指针往后移
+      newStartVnode = newChildren[++newStartIndex] // 把指针往后移
+    
+    // 2) 尾比尾相同 新旧尾指针指向的相同
+    } else if(isSameVnode(oldEndVnode, newEndVnode)) {
+      patch(oldEndVnode, newEndVnode)
+      oldEndVnode = oldChildren[--oldEndIndex]
+      newEndVnode = newChildren[--newEndIndex]
+
+    // 3) 头比尾相同 倒叙 需要做移动操作
+    } else if(isSameVnode(oldStartVnode, newEndVnode)) {
+      patch(oldStartVnode, newEndVnode);
+      // 移动oldStartVnode 到oldEndVnode 后边
+      parent.insertBefore(oldStartVnode.el, oldEndVnode.el.nextSibling) // nextSibling是下一个元素
+      // 更新指针
+      oldStartVnode = oldChildren[++oldStartIndex]
+      newEndVnode = newChildren[--newEndIndex]
+    
+    // 4) 尾比头相同 
+    } else if(isSameVnode(oldEndVnode, newStartVnode)) {
+      patch(oldEndVnode, newStartVnode);
+      // 移动oldEndVnode 到oldStartVnode 前边
+      parent.insertBefore(oldEndVnode.el, oldStartVnode.el)
+      // 更新指针
+      oldEndVnode = oldChildren[--oldEndIndex]
+      newStartVnode = newChildren[++newStartIndex]
+    
+    // 5) 两个列表 是乱序 也无复用节点
+    } else {
+      
+    }
+
+  }
+
+  // 如果到最后还有剩余, 需要将剩余的插入
+  if(newStartIndex <= newEndIndex) { // 新的两个指针还没重合, 说明新的孩子多
+    for(let i = newStartIndex; i <= newEndIndex; i++) {
+      // 可能是往前边插 也可能是往后边插
+      // 设定要参考元素: 指针往后挪一个, patch的时候 已经往newVnode上挂载了el属性
+      let ele = newChildren[newEndIndex + 1] == null ? null : newChildren[newEndIndex + 1].el
+      // newChildren[i]是剩下的元素 ,没有patch过 所以没有el属性
+      parent.insertBefore(createElm(newChildren[i]),ele)
+
+      // insertBefore(插入的元素, null) = appendChild()
+      // parent.appendChild(createElm(newChildren[i]))
+    }
+  }
+
+}
+
+// 判断新旧虚拟节点 是否相同
+function isSameVnode(oldVnode, newVnode) {
+  // 如果标签和key都一样 则认为是同一个节点, 两个虚拟节点一样则可以复用真实节点
+  return (oldVnode.tag === newVnode.tag) && (oldVnode.key === newVnode.key)
 }
